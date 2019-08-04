344082752d5585164c21394de5a4004b39f91c76
Martin Klomp <klompmartin@hotmail.com>
2019-08-04 23:30:42 +0200

    Blockly div resizing

--- app/public/blockly.js
+++ app/public/blockly.js
@@ -5,6 +5,8 @@ Blockly.Python.finish = function(code) {
 };
 
 
+
+
   Blockly.BlockSvg.START_HAT = true;
 
 
@@ -78,7 +80,8 @@ Blockly.Python.finish = function(code) {
   //var xml = Blockly.Xml.textToDom(localStorage.getItem("blockly"));
   var demoWorkspace = Blockly.inject('blocklyDiv',
         {
-         toolbox: document.getElementById('toolbox')});
+         toolbox: document.getElementById('toolbox')
+        });
 
     // if xml
     //Blockly.Xml.domToWorkspace(xml, demoWorkspace);
@@ -111,5 +114,31 @@ Blockly.Python.finish = function(code) {
 
       }
     });
+
+
+
+    var blocklyArea = document.getElementById('blocklyArea');
+    var blocklyDiv = document.getElementById('blocklyDiv');
+
+    var onresize = function(e) {
+      // Compute the absolute coordinates and dimensions of blocklyArea.
+      var element = blocklyArea;
+      var x = 0;
+      var y = 0;
+      do {
+        x += element.offsetLeft;
+        y += element.offsetTop;
+        element = element.offsetParent;
+      } while (element);
+      // Position blocklyDiv over blocklyArea.
+      blocklyDiv.style.left = x + 'px';
+      blocklyDiv.style.top = y + 'px';
+      blocklyDiv.style.width = blocklyArea.offsetWidth + 'px';
+      blocklyDiv.style.height = blocklyArea.offsetHeight + 'px';
+      Blockly.svgResize(demoWorkspace);
+    };
+    window.addEventListener('resize', onresize, false);
+    onresize();
+    Blockly.svgResize(demoWorkspace);
 
 
