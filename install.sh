83b222e1dc904be896040bf5964c604a88cdbb99
Martin Klomp <m.klomp@tudelft.nl>
2020-05-19 10:35:46 +0200

    Temp fix for iptables and chroot

--- install.sh
+++ install.sh
@@ -27,8 +27,22 @@ sudo ln -s $ZOEF_SRC_DIR/web_interface/python/linetrace.py /home/zoef/workdir
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
 sudo apt install -y iptables-persistent
-sudo iptables -A PREROUTING -t nat -p tcp --dport 80 -j REDIRECT --to-port 3000
-sudo bash -c "iptables-save > /etc/iptables/rules.v4"
+#sudo iptables -A PREROUTING -t nat -p tcp --dport 80 -j REDIRECT --to-port 3000
+#sudo bash -c "iptables-save > /etc/iptables/rules.v4"
+
+# TODO: iptables(-save) do now work from chroot, so saving it manually
+sudo echo "*filter" > /etc/iptables/rules.v4
+sudo echo ":INPUT ACCEPT [1042:276951]" >> /etc/iptables/rules.v4
+sudo echo ":FORWARD ACCEPT [0:0]" >> /etc/iptables/rules.v4
+sudo echo ":OUTPUT ACCEPT [1039:277247]" >> /etc/iptables/rules.v4
+sudo echo "COMMIT" >> /etc/iptables/rules.v4
+sudo echo "*nat" >> /etc/iptables/rules.v4
+sudo echo ":PREROUTING ACCEPT [1:169]" >> /etc/iptables/rules.v4
+sudo echo ":INPUT ACCEPT [1:169]" >> /etc/iptables/rules.v4
+sudo echo ":OUTPUT ACCEPT [3:905]" >> /etc/iptables/rules.v4
+sudo echo ":POSTROUTING ACCEPT [3:905]" >> /etc/iptables/rules.v4
+sudo echo "-A PREROUTING -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 3000" >> /etc/iptables/rules.v4
+sudo echo "COMMIT" >> /etc/iptables/rules.v4
 
 # Add systemd service
 sudo rm /lib/systemd/system/zoef_web_interface.service
