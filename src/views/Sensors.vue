10cfcb834b331d0eec2c4a6b85fe5064885158ce
Mees Brinkhuis <m.brinkhuis@student.tudelft.nl>
2020-08-05 20:13:47 +0000

    Add other sensors to sensor page

--- src/views/Sensors.vue
+++ src/views/Sensors.vue
@@ -20,6 +20,46 @@
         </td>
       </tr>
     </table>
+    
+    <h3 class="mt-5">Lichtsensors</h3>
+    <table>
+      <tr>
+        <td>
+          left
+        </td>
+        <td>
+          {{left_intensity}}
+        </td>
+      </tr>
+      <tr>
+        <td>
+          right
+        </td>
+        <td>
+          {{right_intensity}}
+        </td>
+      </tr>
+    </table>
+    
+    <h3 class="mt-5">Afstand</h3>
+    <table>
+      <tr>
+        <td>
+          left
+        </td>
+        <td>
+          {{left_distance}}
+        </td>
+      </tr>
+      <tr>
+        <td>
+          right
+        </td>
+        <td>
+          {{right_distance}}
+        </td>
+      </tr>
+    </table>
 
   </div>
 </template>
@@ -32,7 +72,11 @@ export default {
   data() {
     return {
       left_encoder: 0,
-      right_encoder: 0
+      right_encoder: 0,
+      left_distance: 0,
+      right_distance: 0
+      left_intensity: 0,
+      right_intensity: 0
     }
   },
 
@@ -55,6 +99,30 @@ export default {
       name : '/zoef/right_encoder',
       messageType : 'zoef_msgs/Encoder'
     });
+    
+    var left_intensity_sub = new ROSLIB.Topic({
+      ros : ros,
+      name : '/zoef/left_encoder',
+      messageType : 'zoef_msgs/Intensity'
+    });
+
+    var right_intensity_sub = new ROSLIB.Topic({
+      ros : ros,
+      name : '/zoef/right_encoder',
+      messageType : 'zoef_msgs/Intensity'
+    });
+    
+    var left_distance_sub = new ROSLIB.Topic({
+      ros : ros,
+      name : '/zoef/left_distance',
+      messageType : 'sensor_msgs/Range'
+    });
+
+    var right_distance_sub = new ROSLIB.Topic({
+      ros : ros,
+      name : '/zoef/right_distance',
+      messageType : 'sensor_msgs/Range'
+    });
 
     left_encoder_sub.subscribe((message) => {
       this.left_encoder = message.value
@@ -63,6 +131,24 @@ export default {
     right_encoder_sub.subscribe((message) => {
       this.right_encoder = message.value
     });
+    
+    left_intensity_sub.subscribe((message) => {
+      this.left_intensity = message.value
+    });
+
+    right_intensity_sub.subscribe((message) => {
+      this.right_intensity = message.value
+    });
+
+
+    left_distance_sub.subscribe((message) => {
+      this.left_distance = message.value
+    });
+
+    right_distance_sub.subscribe((message) => {
+      this.right_distance = message.value
+    });
+
 
   }
 }
