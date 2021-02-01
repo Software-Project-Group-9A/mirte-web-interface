75741eef267e9d38485e3812c6294cd897b592f2
Martin Klomp <m.klomp@tudelft.nl>
2021-02-01 18:50:44 +0000

    Changed background of terminal to more friendly mode

--- src/components/Xterm.vue
+++ src/components/Xterm.vue
@@ -96,9 +96,11 @@ export default {
         },
         setTerminal(terminal){
            if (terminal){
+              this.term.setOption('theme', {});
               this.shell_socket.send("stty echo && PS1='\\[\\e]0;\\u@\\h: \\w\\a\\]${debian_chroot:+($debian_chroot)}\\[\\033[01;32m\\]\\u@\\h\\[\\033[00m\\]:\\[\\033[01;34m\\]\\w\\[\\033[00m\\]\\$ ' && clear\n");
               this.term.setOption('disableStdin', false);
            } else {
+              this.term.setOption('theme', { background: '#e2e8e9', foreground: '#000000' });
               this.shell_socket.send("stty -echo && PS1='' && clear\n");
               this.shell_socket.send("clear\n");
               this.term.setOption('disableStdin', true);
