8745c71f11b6c9bb43c7a08efc68e26a53734dba
Martin Klomp <m.klomp@tudelft.nl>
2021-10-27 15:45:48 +0200

    Fixing issue with missing blockly media

--- vue-frontend/vue.config.js
+++ vue-frontend/vue.config.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
 module.exports = {
   pluginOptions: {
     i18n: {
@@ -6,5 +9,17 @@ module.exports = {
       localeDir: 'locales',
       enableInSFC: true
     }
+  },
+  publicPath: './',
+  configureWebpack: {
+    plugins: [
+      // Copy over media resources from the Blockly package
+      new CopyPlugin([
+        {
+          from: path.resolve(__dirname, './node_modules/blockly/media'),
+          to: path.resolve(__dirname, 'dist/blockly-media')
+        }
+      ])
+    ]
   }
 }
