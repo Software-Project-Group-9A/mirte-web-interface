cc83baaa789ff238b2eeda9418b45525d804e5da
Tijs Lenssen <tijslenssen@icloud.com>
2022-05-31 12:34:07 +0200

    Added option to save html

--- vue-frontend/src/views/WebsiteEditor.vue
+++ vue-frontend/src/views/WebsiteEditor.vue
@@ -4,7 +4,16 @@
          <div class="col-6 p-2 h-100 offset-3" style="overflow: hidden;"> 
              <div class="layoutbox rounded h-100" style="overflow: hidden; display: flex; flex-flow: column;">
                 <div class="text-white p-2 h3 m-0 layoutbox-title w-100 background-primary">
-                   HTML
+                  HTML
+                  <div style="float: right">
+                     <button class="btn btn-outline-light mr-2" 
+                        v-b-tooltip.hover 
+                        :title="$t('website_editor.save')" 
+                        @click="saveFile"
+                     >
+                     <i class="fa fa-save"></i>
+                     </button>
+                  </div>
                 </div>
                 
                 <div class="h-40" style="min-height: 40%; overflow: auto;" >
@@ -21,8 +30,27 @@
 import HTMLEditor from '@/components/HTMLEditor.vue'
 
 export default {
-  components: {
-    HTMLEditor
-  }
+   components: {
+      HTMLEditor
+   },
+   methods: {
+      saveFile: function() {
+         const htmlUrl = `http://${location.hostname}:3000/api/html`;
+
+         fetch(htmlUrl, {
+               method: 'POST',
+               headers: {
+                  'Content-Type': 'text/plain',
+                  'CORS': 'Access-Control-Allow-Origin'
+               },
+               body: this.$store.getters.getHTML,
+         }).then(res => {
+               this.waitForSocketConnection();
+         }).catch(err => {
+               console.log("sending failed")
+               console.log(err)
+         })
+      }
+   }
 }
 </script>
