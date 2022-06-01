4db355adb511fa08686990dca050854b00015d2f
Pieter Carton <p.a.carton@student.tudelft.nl>
2022-06-01 14:58:33 +0200

    import roslib and sensorlib scripts in ExposeSensors view

--- vue-frontend/src/views/ExposeSensors.vue
+++ vue-frontend/src/views/ExposeSensors.vue
@@ -1,15 +1,13 @@
 <template>
-
       <div class="row p-4 h-100">
          <div class="col-6 p-2 h-100 offset-3" style="overflow: hidden;"> 
              <div class="layoutbox rounded h-100" style="overflow: hidden; display: flex; flex-flow: column;">
                 <div class="text-white p-2 h3 m-0 layoutbox-title w-100 background-primary">
-                   HTML
+                  Expose Sensors
                 </div>
                 
                 <div class="h-40" style="min-height: 40%; overflow: auto;" >
-                Hello There
-                    <HTMLEditor/>
+                  Hello There
                 </div>
              </div>
          </div> 
@@ -18,12 +16,13 @@
 </template>
 
 <script>
+// import roslib and sensorlib
+import ROSLIB from 'roslib'
+import '@/assets/sensorlib/RosSensorLib.min.js';
 
-import HTMLEditor from '@/components/HTMLEditor.vue'
+// for now, just print both to the console
+console.dir(ROSLIB);
+console.dir(SENSORLIB);
 
-export default {
-  components: {
-    HTMLEditor
-  }
-}
+export default {}
 </script>
