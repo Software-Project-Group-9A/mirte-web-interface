03a65f733af6258a3728f1350346835a45b0783b
Pieter Carton <p.a.carton@student.tudelft.nl>
2022-06-07 15:04:05 +0200

    add connection status bar to web interface, make page automatically connect to rosbridge server, auto expose sensors described in config

--- vue-frontend/src/views/ExposeSensors.vue
+++ vue-frontend/src/views/ExposeSensors.vue
@@ -3,9 +3,16 @@
          <div class="col-6 p-2 h-100 offset-3" style="overflow: hidden;"> 
              <div class="layoutbox rounded h-100" style="overflow: hidden; display: flex; flex-flow: column;">
                 <div class="text-white p-2 h3 m-0 layoutbox-title w-100 background-primary">
+                  Status
+                </div>
+                <div id='connection_info' class="h-40" style="min-height: 40%; overflow: auto;" >
+                  Connecting to Mirte...
+                </div>
+             </div>
+             <div class="layoutbox rounded h-100" style="overflow: hidden; display: flex; flex-flow: column;">
+                <div class="text-white p-2 h3 m-0 layoutbox-title w-100 background-primary">
                   Expose Sensors
                 </div>
-                
                 <div class="h-40" style="min-height: 40%; overflow: auto;" >
                   Hello There
                 </div>
@@ -20,9 +27,35 @@
 import ROSLIB from 'roslib'
 import '@/assets/sensorlib/RosSensorLib.min.js';
 
-// for now, just print both to the console
-console.dir(ROSLIB);
-console.dir(SENSORLIB);
+global.ROSLIB = ROSLIB;
+
+    // connect to Mirte
+    const ros = new ROSLIB.Ros();
+
+    // show connection status in status div
+    ros.on('connection', function() {
+      document.getElementById("connection_info").innerHTML = "Connected to webserver.";
+    });
+    ros.on('error', function(error) { 
+      document.getElementById("connection_info").innerHTML = "Failed to connect to webserver."; 
+    });
+    ros.on('close', function() { 
+      document.getElementById("connection_info").innerHTML = 'Connection to websocket server closed.';
+    });
+
+    /* replace by ws(s?)://mirte.local/ros/ws in future */
+    ros.connect('ws://192.168.20.174:9090');
+
+    // receive user config from parameter server
+    const userConfig = new ROSLIB.Param({
+      ros: ros,
+      name: '/mirte',
+    });
+
+    userConfig.get((config) => {
+      // read needed sensors from parameters
+      const map = SENSORLIB.readSensorsFromConfig(config, ros);
+    });
 
 export default {}
 </script>
