e4d908709407d8ef00d2b3b719becd9e1b92f719
Tijs Lenssen <tijslenssen@icloud.com>
2022-06-20 14:05:48 +0200

    Changed imu to phone_imu and changed blocks so they require imu name

--- vue-frontend/src/assets/blockly/phone_imu.js
+++ vue-frontend/src/assets/blockly/phone_imu.js
@@ -4,13 +4,23 @@ export function load (Blockly, instances) {
         instances = [["NO PERIPHERAL CONFIGURED","NO PERIPHERAL CONFIGURED"]]
     }
 
+    Blockly.Extensions.register('dynamic_instances_extension_phone_imu',
+    function() {
+      this.getInput('INSTANCE')
+      .appendField(new Blockly.FieldDropdown(instances), 'INSTANCE');
+    });
+
     // ROTATION BLOCK
-    Blockly.Blocks['get_rotation_imu'] = {
+    Blockly.Blocks['get_rotation_phone_imu'] = {
         init: function () {
             this.jsonInit({
 		  "type": "block_type",
-		  "message0": "%{BKY_IMU_ORIENTATION}",
+		  "message0": "%{BKY_PHONE_IMU_ORIENTATION}",
 		  "args0": [
+        {
+		      "type": "input_dummy",
+		      "name": "INSTANCE"
+		    },
         {
           "type": "field_dropdown",
           "name": "AXIS",
@@ -24,24 +34,30 @@ export function load (Blockly, instances) {
 		  "inputsInline": true,
 		  "colour": "%{BKY_SENSORS_RGB}",
       "output": "Number",
+		  "extensions": ["dynamic_instances_extension_phone_imu"]
             });
         }
     };
 
-    Blockly.Python['get_rotation_imu'] = function (block) {
+    Blockly.Python['get_rotation_phone_imu'] = function (block) {
       Blockly.Python.definitions_['import_mirte'] = 'from mirte_robot import robot\nmirte=robot.createRobot()';
+      let instance = block.getFieldValue('INSTANCE');
       let axis = block.getFieldValue('AXIS');
-      let code = `mirte.getImuRotation('${axis}')`;
+      let code = `mirte.getImuRotation('${instance}', '${axis}')`;
       return [code, Blockly.Python.ORDER_NONE]
     };
 
     // LINEAR ACCELERATION BLOCK
-    Blockly.Blocks['get_linear_acceleration_imu'] = {
+    Blockly.Blocks['get_linear_acceleration_phone_imu'] = {
         init: function () {
             this.jsonInit({
 		  "type": "block_type",
-		  "message0": "%{BKY_IMU_LINEAR_ACCELERATION}",
+		  "message0": "%{BKY_PHONE_IMU_LINEAR_ACCELERATION}",
 		  "args0": [
+        {
+		      "type": "input_dummy",
+		      "name": "INSTANCE"
+		    },
 		    {
           "type": "field_dropdown",
           "name": "AXIS",
@@ -54,25 +70,31 @@ export function load (Blockly, instances) {
 		  ],
 		  "inputsInline": true,
 		  "colour": "%{BKY_SENSORS_RGB}",
-      "output": "Number"
+      "output": "Number",
+		  "extensions": ["dynamic_instances_extension_phone_imu"]
             });
         }
     };
 
-    Blockly.Python['get_linear_acceleration_imu'] = function (block) {
+    Blockly.Python['get_linear_acceleration_phone_imu'] = function (block) {
       Blockly.Python.definitions_['import_mirte'] = 'from mirte_robot import robot\nmirte=robot.createRobot()';
+      let instance = block.getFieldValue('INSTANCE');
       let axis = block.getFieldValue('AXIS');
-      let code = `mirte.getImuLinearAcceleration('${axis}')`;
+      let code = `mirte.getImuLinearAcceleration('${instance}', '${axis}')`;
       return [code, Blockly.Python.ORDER_NONE]
     };
 
     // ANGULAR VELOCITY BLOCK
-    Blockly.Blocks['get_angular_velocity_imu'] = {
+    Blockly.Blocks['get_angular_velocity_phone_imu'] = {
         init: function () {
             this.jsonInit({
 		  "type": "block_type",
-		  "message0": "%{BKY_IMU_ANGULAR_VELOCITY}",
+		  "message0": "%{BKY_PHONE_IMU_ANGULAR_VELOCITY}",
 		  "args0": [
+        {
+		      "type": "input_dummy",
+		      "name": "INSTANCE"
+		    },
 		    {
           "type": "field_dropdown",
           "name": "AXIS",
@@ -85,15 +107,17 @@ export function load (Blockly, instances) {
 		  ],
 		  "inputsInline": true,
 		  "colour": "%{BKY_SENSORS_RGB}",
-      "output": "Number"
+      "output": "Number",
+		  "extensions": ["dynamic_instances_extension_phone_imu"]
             });
         }
     };
 
-    Blockly.Python['get_angular_velocity_imu'] = function (block) {
+    Blockly.Python['get_angular_velocity_phone_imu'] = function (block) {
       Blockly.Python.definitions_['import_mirte'] = 'from mirte_robot import robot\nmirte=robot.createRobot()';
+      let instance = block.getFieldValue('INSTANCE');
       let axis = block.getFieldValue('AXIS');
-      let code = `mirte.getImuAngularVelocity('${axis}')`;
+      let code = `mirte.getImuAngularVelocity('${instance}', '${axis}')`;
       return [code, Blockly.Python.ORDER_NONE]
     };
 }
